b7590d194b36f46ce1b789f112d7d2dfb1c9657b
root <root@dolby.ury.york.ac.uk>
2018-04-02 16:47:31 +0100

    Add source number to silence emails

--- scripts/critical_blank.php
+++ scripts/critical_blank.php
@@ -3,7 +3,7 @@
 <?php
 
 $silencedisable = `head -n 1 /usr/local/etc/ury/silencedisable`;
-$oldstate = `/usr/local/bin/sel raw`;
+$oldstate = intval(`/usr/local/bin/sel raw`);
 
 $headers = 'From: Liquidsoap <liquidsoap@ury.org.uk>';
 
@@ -12,20 +12,20 @@ if ($silencedisable == 0){
     if($oldstate == 3) {
         mail('computing@ury.org.uk',
             'Critical Silence Alert (Jukebox)',
-            'URY has been switched to jukebox due to a breach of the silence threshold. At the time, jukebox was on-air.',
+            'URY has been switched to Jukebox due to a breach of the silence threshold. At the time, Jukebox was on air.',
             $headers);
     } else {
         $to = ['chief.engineer@ury.org.uk', 'computing@ury.org.uk', 'pc@ury.org.uk'];
         $subject = '[URY] Silence Alert';
-        $message = '30 seconds of quiet output has been detected on the web stream. Jukebox has been automatically put to air.';
+        $message = '30 seconds of silence was detected on live output. Source '.$oldstate.' was on air. Jukebox was automatically selected.';
         foreach ($to as $r) {
-            mail($r, $subject, $message, $headers);
+           mail($r, $subject, $message, $headers);
         }
     }
 } elseif ($silencedisable == 1) {
-    $to = ['chief.engineer@ury.org.uk', 'computing@ury.org.uk'];
+    $to = ['computing@ury.org.uk'];
     $subject = '[URY] Silence Alert - Switch Disabled';
-    $message = 'The silence threshold was breached, but auto-switch was disabled. (Selector input remains on $oldstate)';
+    $message = 'The silence threshold was breached, but auto-switch was disabled. Selector input remains on source '.$oldstate.'.';
     foreach ($to as $r) {
         mail($r, $subject, $message, $headers);
     }
